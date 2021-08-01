6b5f10e45cc633e929f50fded1f134dc553c3995
BrentWallace <brent.wallace@gofishdigital.com>
2021-08-01 15:39:43 -0400

    built out game model

--- src/models/game.js
+++ src/models/game.js
@@ -1,1 +1,24 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const gameSchema = new Schema ({
+  game_id: String,
+  season: String,
+  type: String,
+  date_time_gmt: String,
+  away_team_id: String,
+  home_team_id: String,
+  away_goals: String,
+  home_goals: String,
+  outcome: String,
+  home_rink_side_start: String,
+  venue: String,
+  venue_link: String,
+  venue_time_zone_id: String,
+  venue_time_zone_offset: String,
+  venue_time_zone_tz: String,
+});
+
+const Player = mongoose.model('Player', playerSchema);
+
+module.exports = Player;
