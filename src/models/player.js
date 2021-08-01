fa887df26d9222087fdcd2b737f116d482565f29
BrentWallace <brent.wallace@gofishdigital.com>
2021-08-01 15:43:10 -0400

    built player model

--- src/models/player.js
+++ src/models/player.js
@@ -2,9 +2,18 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const playerSchema = new Schema ({
-  playerId: Number,
+  player_id: String,
   firstName: String,
-  LastName: String,
+  lastName: String,
+  nationality: String,
+  birthCity: String,
+  primaryPosition: String,
+  birthDate: String,
+  birthStateProvince: String,
+  height: String,
+  height_cm: String,
+  weight: String,
+  shootsCatches: String,
 });
 
 const Player = mongoose.model('Player', playerSchema);
